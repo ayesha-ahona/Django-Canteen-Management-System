7efd7bee21b12ffcef9bcfca1e6c1634f83aac71
proma-krity <promakrity89@gmail.com>
2025-11-15 21:30:37 +0600

    Add date and time feature in daily sales

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,14 +113,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'   
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = True              
+
 
 
 # Static files (CSS, JavaScript, Images)
